6345e5e2c268ff7962711cd649781972dbafedd9
Brendan Rogers <brendanr11@gmail.com>
2018-09-12 13:12:57 -0400

    Began development of responsive sources for IMGs and VIDEOs

--- pure_volume/js/main.js
+++ pure_volume/js/main.js
@@ -2,12 +2,13 @@
 (() => {
   console.log("Linked");
 
-  //VARIABLE STACK
+  // VARIABLE STACK
   let play = document.querySelector('.play'),
       pause = document.querySelector('.pause'),
       mute = document.querySelector('.mute'),
       unmute = document.querySelector('.unmute'),
-      bannerVid = document.querySelector('.bannerVid');
+      bannerVid = document.querySelector('.bannerVid'),
+      desktopQ = window.matchMedia("(min-width: 1024px)");
 
 
   //FUNCTIONS
@@ -40,10 +41,33 @@
     }
   }
 
+  function reassignImages(x) {
+    if (x.matches) { // If media query matches
+        document.getElementById('drake').src='images/desktop_images/drake.png'
+        document.getElementById('gucci').src='images/desktop_images/gucci.png'
+        document.getElementById('john').src='images/desktop_images/john.png'
+        document.getElementById('niel').src='images/desktop_images/niel.png'
+        document.getElementById('alicia').src='images/desktop_images/alicia.png'
+        document.getElementById('star').src='images/desktop_images/star.png'
+        document.getElementById('ryan').src='images/desktop_images/ryan.png'
+        document.getElementById('car').src='images/desktop_images/car.png'
+        document.getElementById('paul').src='images/desktop_images/paul.png'
+        document.getElementById('pump').src='images/desktop_images/pump.png'
+        document.getElementById('trippie').src='images/desktop_images/trippie.png'
+        document.getElementById('tekashi').src='images/desktop_images/tekashi.png'
+    } else {
+    
+    }
+  }
 
-  //EVENT HANDLING
+  // call FUNCTIONS
+  reassignImages(desktopQ);
+
+  // EVENT HANDLING
   pause.addEventListener('click', pausePlay);
   play.addEventListener('click', pausePlay);
   unmute.addEventListener('click', muted);
   mute.addEventListener('click', muted);
+  desktopQ.addListener(reassignImages);
+
 })();
